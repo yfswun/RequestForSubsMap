517ee6f97b76dcba350fbcf664359438117fe731
Sylvia Wun <yfswun@yahoo.com>
2015-07-02 17:59:22 -0700

    JobSiteLoc: Completed change of ad_location to ad_siteaddress

--- Constants.php
+++ Constants.php
@@ -18,29 +18,37 @@ public static function getCommonCriteria() {
 . " and datediff(curdate(), date_created) < 32"
 
    // exclude email, fax, multiple, and various
-. " and lower(trim(A.ad_location)) not like '%email%'"
-. " and lower(trim(A.ad_location)) not like '%fax%'"
-. " and lower(A.ad_location) not regexp 'multiple|various'"
+// . " and lower(trim(A.ad_location)) not like '%email%'"
+. " and lower(trim(A.ad_siteaddress)) not like '%email%'"
+// . " and lower(trim(A.ad_location)) not like '%fax%'"
+. " and lower(trim(A.ad_siteaddress)) not like '%fax%'"
+// . " and lower(A.ad_location) not regexp 'multiple|various'"
+. " and lower(A.ad_siteaddress) not regexp 'multiple|various'"
 
    // exclude some known general locations
-. " and lower(trim(A.ad_location)) not in"
+// . " and lower(trim(A.ad_location)) not in"
+. " and lower(trim(A.ad_siteaddress)) not in"
 . "     (  'united states', 'ca', 'california', 'bay area'"
 . "      , 'san francisco, ca', 'san francisco ca', 'san francisco', 'sf')"
 
    // exclude city/county only
-. " and lower(trim(A.ad_location)) not regexp '^[[:alpha:] ]*,?[ ]*[a-z]{2}[ ]?[[:digit:]]{0,5}$'"
-. " and lower(trim(A.ad_location)) not regexp '^city of .+$'"
-. " and lower(trim(A.ad_location)) not regexp '^.*county$'"
+// . " and lower(trim(A.ad_location)) not regexp '^[[:alpha:] ]*,?[ ]*[a-z]{2}[ ]?[[:digit:]]{0,5}$'"
+// . " and lower(trim(A.ad_location)) not regexp '^city of .+$'"
+// . " and lower(trim(A.ad_location)) not regexp '^.*county$'"
+. " and (   A.ad_siteaddress is null"
+. "      or A.ad_siteaddress = ''"
 
    // exclude phone number
    // http://stackoverflow.com/questions/16699007/regular-expression-to-match-standard-10-digit-phone-number
-. " and trim(A.ad_location) not regexp"
+// . " and trim(A.ad_location) not regexp"
+. " and trim(A.ad_siteaddress) not regexp"
 . "       '^("
 . "\\\\" // 2 slashes in regexp
 . "+[[:digit:]]{1,2} )?[[.(.]]?[[:digit:]]{3}[[.).]]?[ .-]?[[:digit:]]{3}[ .-]?[[:digit:]]{4}$'"
 
    // only 7 digits with optional separators
-. " and trim(A.ad_location) not regexp '^[[:digit:]]{3}[ .-]?[[:digit:]]{4}$'"
+// . " and trim(A.ad_location) not regexp '^[[:digit:]]{3}[ .-]?[[:digit:]]{4}$'"
+. " and trim(A.ad_siteaddress) not regexp '^[[:digit:]]{3}[ .-]?[[:digit:]]{4}$'"
 ;
 
 return $commonCriteria;
